68cf22f8a81989fc87119eb06b5fa64bbfcbcc87
Fabrice Reix <fabrice.reix@gmail.com>
2019-03-31 09:20:21 +0200

    Update

--- spec/templates/Svg/AttributeParser.hs
+++ spec/templates/Svg/AttributeParser.hs
@@ -7,6 +7,6 @@ type Error = String
 
 attribute :: Element -> Attribute -> Either Error Element
 {% for element in elements %}
-{% for attribute in element.attributes %}atttribute elem@({{element.name|capitalize}}) ("{{attribute.name}}", value) = Right elem
-{% endfor %}atttribute elem@({{element.name|capitalize}}) {% for n in range(7-element.name|length) %} {% endfor %}(name, _) = Left $ "Invalid attribute " ++ name ++ " for element " ++ {{element.name|capitalize}}
+{% for attribute in element.attributes %}atttribute elem@({{element.name|capitalize}}) ("{{attribute.name}}", value) {% for n in range(10-attribute.name|length) %} {% endfor %}= Right elem
+{% endfor %}atttribute elem@({{element.name|capitalize}}) (name, _)             = Left $ "Invalid attribute " ++ name ++ " for element " ++ {{element.name|capitalize}}
 {% endfor %}
