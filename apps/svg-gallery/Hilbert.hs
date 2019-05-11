43ad7f16abda9e244ab132e44a7d135fc0876952
Fabrice Reix <fabrice.reix@gmail.com>
2019-05-11 08:09:22 +0200

    Optimize Path

--- apps/svg-gallery/Hilbert.hs
+++ apps/svg-gallery/Hilbert.hs
@@ -40,7 +40,7 @@ diagramPath n = fromRight $ Right Default.svg
                                    >>= Path.strokewidth 0.05
                                    >>= Path.stroke "red"
                                    >>= Path.fill "none"
-                                   >>= Path.d (M False 0 0:map toPathCommand (diffPoints $ hilbert n))
+                                   >>= Path.d (optimizePath (M False 0 0:map toPathCommand (diffPoints $ hilbert n)))
                       ]
 
 -- Helpers
@@ -78,6 +78,24 @@ diffPoints :: [Point]->[(Int,Int)]
 diffPoints [] = []
 diffPoints [_] = []
 diffPoints ((x1,y1):(x2,y2):ps) = (x2-x1,y2-y1):diffPoints ((x2,y2):ps)
+
+
+optimizePath :: [Command] -> [Command]
+optimizePath [] = []
+optimizePath [c] = [c]
+optimizePath (c1:c2:cs) = case mergeCommands c1 c2 of
+   [_,_] -> c1:optimizePath (c2:cs)
+   c  -> optimizePath (c ++ cs)
+
+
+mergeCommands :: Command -> Command -> [Command]
+mergeCommands c1@(H True x1) c2@(H True x2) = if signum x1 == signum x2
+                                              then [H True (x1+x2)]
+                                              else [c1,c2]
+mergeCommands c1@(V True x1) c2@(V True x2) = if signum x1 == signum x2
+                                              then [V True (x1+x2)]
+                                              else [c1,c2]
+mergeCommands c1 c2 = [c1, c2]
 
 
 
