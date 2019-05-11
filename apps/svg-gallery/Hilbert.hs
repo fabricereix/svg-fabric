e9f5079524484a982c6df3ffdf738e933b4673db
Fabrice Reix <fabrice.reix@gmail.com>
2019-05-11 07:40:42 +0200

    Generate path version

--- apps/svg-gallery/Hilbert.hs
+++ apps/svg-gallery/Hilbert.hs
@@ -3,17 +3,23 @@ import Text.XML
 import qualified Svg.DefaultElements as Default
 import qualified Svg.Setter.Svg as Svg
 import qualified Svg.Setter.Polyline as Polyline
+import qualified Svg.Setter.Path as Path
 import           Svg.Setter
+import           Svg.Types.Core
 
 
 -- diagram
 type Filename = String
 diagrams :: [(Filename, Element)]
-diagrams = map (\n->("hilbert-" ++ show n ++ ".svg", diagram n)) [1..8]
+diagrams = concatMap (\n->[
+        ("hilbert-polyline-" ++ show n ++ ".svg", diagramPolyline n)
+      , ("hilbert-path-" ++ show n ++ ".svg", diagramPath n)
+      ]) [1..7]
 
 
-diagram :: Int -> Element
-diagram n = fromRight $ Right Default.svg
+
+diagramPolyline :: Int -> Element
+diagramPolyline n = fromRight $ Right Default.svg
                 >>= Svg.width 300
                 >>= Svg.height 300
                 >>= Svg.viewBox 0 0 (2^n-1) (2^n-1)
@@ -24,11 +30,28 @@ diagram n = fromRight $ Right Default.svg
                                    >>= Polyline.stroke "red"
                                    >>= Polyline.points (map toDoublePoint $ hilbert n)
                       ]
+diagramPath :: Int -> Element
+diagramPath n = fromRight $ Right Default.svg
+                >>= Svg.width 300
+                >>= Svg.height 300
+                >>= Svg.viewBox 0 0 (2^n-1) (2^n-1)
+                >>= addChildren [
+                      fromRight $ Right Default.path
+                                   >>= Path.strokewidth 0.05
+                                   >>= Path.stroke "red"
+                                   >>= Path.fill "none"
+                                   >>= Path.d (M False 0 0:map toPathCommand (diffPoints $ hilbert n))
+                      ]
 
 -- Helpers
 type Point = (Int,Int)
 toDoublePoint :: Point -> (Double, Double)
 toDoublePoint (x,y) = (fromIntegral x, fromIntegral y)
+
+toPathCommand :: (Int,Int) -> Command
+toPathCommand (0,y) = V True (fromIntegral y)
+toPathCommand (x,0) = H True (fromIntegral x)
+toPathCommand (x,y) = L True (fromIntegral x) (fromIntegral y)
 
 fromRight :: Show l => Either l r -> r
 fromRight (Left e) = error $ show e
@@ -48,16 +71,13 @@ reverseList :: [a] -> [a]
 reverseList  [] = []
 reverseList  xs = last xs : reverseList (init xs)
 
-part1 :: [Point]->[Point]
-part1 = reverseList
 
 
 
-test_1 :: IO()
-test_1 = do
-  putStrLn "hilbert1"
-  putStrLn "hilbert2"
-
+diffPoints :: [Point]->[(Int,Int)]
+diffPoints [] = []
+diffPoints [_] = []
+diffPoints ((x1,y1):(x2,y2):ps) = (x2-x1,y2-y1):diffPoints ((x2,y2):ps)
 
 
 
