49e34993a25b57043d6cf8367c14020dbd8a0366
Fabrice Reix <fabrice.reix@gmail.com>
2019-05-20 13:22:16 +0200

    Clean

--- apps/svg-gallery/Hanoi.hs
+++ apps/svg-gallery/Hanoi.hs
@@ -17,19 +17,21 @@ diagrams = [("hanoi.svg", diagram)]
 
 diagram :: Element
 diagram = fromRight $ Right Default.svg
-                >>= Svg.width 500
-                >>= Svg.height 500
-                >>= Svg.viewBox 0 0 40 (fromIntegral (10 * (1+length hanoiSequence)))
-                >>= Svg.stroke "black"
-                >>= Svg.strokewidth 0.05
-                >>= addChildren (map (\(i,xs)->stateElement (i*10) (map createStack xs)) $ zip [1..] hanoiSequence)
-                -- >>= addChildren (map (\(i,xs)->stacks (i*10) (map createDisk s1, map createDisk s2, map createDisk s3)) $ zip [1..] hanoiSequence)
-                      --     stacks 10 (stack1,stack2,[])
-                      --   , stacks 20 ([],stack2,[])
+            >>= Svg.width 500
+            >>= Svg.height 500
+            >>= Svg.viewBox 0 0 40 (fromIntegral (10 * (1+length hanoiSequence)))
+            >>= Svg.stroke "black"
+            >>= Svg.strokewidth 0.05
+            >>= addChildren (
+                  map (\(i,xs)->stateElement (i*10) (map createStack xs)) $ zip [1..] hanoiSequence
+                )
 
---hanoi :: Element
---hanoi = fromRight $ Right Default.g
---        >>= addChildren (stacks 10 (stack1,stack2,[]))
+
+
+data Disk = Disk {size :: Double, color :: String }
+type Stack = [Disk]
+type State = [Stack]
+type Pos = (Double,Double)
 
 createDisk :: Int -> Disk
 createDisk 0 = Disk 3 "white"
@@ -38,34 +40,22 @@ createDisk 2 = Disk 7 "blue"
 createDisk 3 = Disk 9 "green"
 createDisk _ = error "invalid disk number"
 
-stack1,stack2 :: Stack
-stack1 = createStack [0,1,2,3]
-stack2 = createStack [0,3]
-
 createStack :: [Int] -> Stack
 createStack = map createDisk
-
-type Pos = (Double,Double)
-
-data Disk = Disk {
-    size :: Double
-  , color :: String
-  }
-type Stack = [Disk]
-type State = [Stack]
 
 
 stateElement :: Double -> State -> Element
 stateElement y xs = fromRight $ Right Default.g
-        >>= addChildren (concatMap (\(i,s)->stackElements (i*10,y) s) $ zip [1..] xs)
+        >>= addChildren (concatMap (\(i,s)->stackElements (i*distanceBetweenStacks,y) s) $ zip [1..] xs)
+   where distanceBetweenStacks = 10
 
 stackElements :: Pos -> Stack -> [Element]
 stackElements (x,y) disks =
     fromRight (Right Default.rect
-             >>= Rect.x x
-             >>= Rect.y y
-             >>= Rect.width diskThickness
-             >>= Rect.height pegHeight
+          >>= Rect.x x
+          >>= Rect.y y
+          >>= Rect.width diskThickness
+          >>= Rect.height pegHeight
   ):map (\(i,Disk s c)->fromRight $ Right Default.rect
           >>= Rect.x (x-(s-1)/2)
           >>= Rect.y (y+pegHeight+i-fromIntegral (length disks) -1)
@@ -77,11 +67,8 @@ stackElements (x,y) disks =
          diskThickness = 1
 
 
-
-
-
 -- Core
-hanoiSequence :: [[[Int]]] -- Stacks of Disks
+hanoiSequence :: [[[Int]]] -- States of Stacks of Disks
 hanoiSequence =  [[[0,1,2,3],[],[]],[[1,2,3],[0],[]],[[2,3],[0],[1]],[[2,3],[],[0,1]],[[3],[2],[0,1]],[[0,3],[2],[1]],[[0,3],[1,2],[]],[[3],[0,1,2],[]],[[],[0,1,2],[3]],[[],[1,2],[0,3]],[[1],[2],[0,3]],[[0,1],[2],[3]],[[0,1],[],[2,3]],[[1],[0],[2,3]],[[],[0],[1,2,3]],[[],[],[0,1,2,3]]]
 
 
