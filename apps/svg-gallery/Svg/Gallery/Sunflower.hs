0816a7f84e8fff594a3538e878ebce17abe8020b
Fabrice Reix <fabrice.reix@gmail.com>
2019-05-21 13:17:38 +0200

    Update

--- apps/svg-gallery/Svg/Gallery/Sunflower.hs
+++ apps/svg-gallery/Svg/Gallery/Sunflower.hs
@@ -2,7 +2,7 @@ module Svg.Gallery.Sunflower where
 import qualified Svg.DefaultElements as Default
 import qualified Svg.Setter.Svg      as Svg
 import           Text.XML
--- import qualified Svg.Setter.Rect as Rect
+import qualified Svg.Setter.Circle as Circle
 -- import qualified Svg.Setter.G as G
 import           Svg.Setter
 -- import           Svg.Types.Core
@@ -12,21 +12,35 @@ import           Svg.Gallery.Helper
 -- diagram
 type Filename = String
 diagrams :: [(Filename, Element)]
-diagrams = map (\n-> ("sunflower" ++ show n ++ ".svg", diagram n)) [10]
+diagrams = map (\n-> ("sunflower-" ++ show n ++ ".svg", diagram n)) [10,100,1000]
 
 
 
-diagram :: Int -> Element
-diagram _ = fromRight $ Right Default.svg
+diagram  :: Int -> Element
+diagram n = fromRight $ Right Default.svg
             >>= Svg.width 500
             >>= Svg.height 500
-            >>= Svg.viewBox (-20) (-20) 40 40
+            >>= Svg.viewBox (-size) (-size) (2*size) (2*size)
             >>= Svg.stroke "black"
             >>= Svg.strokewidth 0.05
-            >>= addChildren []
+            >>= addChildren (
+                 fromRight ( Right (floret (0,0)) >>= Circle.fill "red")
+                 :map (\i-> floret (sqrt (fromIntegral i), fromIntegral i*goldenAngle)) [1..n]
+                )
+     where size = 1 + fromIntegral (ceiling (sqrt $ fromIntegral n :: Double) :: Int)
+
+goldenAngle :: Double
+goldenAngle = pi * (3 - sqrt 5) -- 2.4
 
 
+fromPolar :: Double -> Double -> (Double,Double)
+fromPolar r theta = (r * cos theta, r * sin theta)
 
 
-
+floret :: (Double, Double) -> Element
+floret (r,theta) = fromRight $ Right Default.circle
+                     >>= Circle.cx x
+                     >>= Circle.cy (-y)
+                     >>= Circle.r 0.6
+    where (x,y) = fromPolar r theta
 
