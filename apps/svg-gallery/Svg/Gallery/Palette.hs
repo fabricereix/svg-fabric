2428aa11b079040e08b60daade6d792489eee7e0
Fabrice Reix <fabrice.reix@gmail.com>
2019-05-25 16:15:42 +0200

    get text out of the graphics

--- apps/svg-gallery/Svg/Gallery/Palette.hs
+++ apps/svg-gallery/Svg/Gallery/Palette.hs
@@ -31,21 +31,34 @@ palettes = fromRight $ Right Default.svg
             >>= Svg.strokewidth 0.05
             >>= addChildren [
                   style
-                , addTransform [Translate 0.5 0.5] $ rectangularPalette "Oranges" brewerOranges
-                , addTransform [Translate 10 0.5] $ rectangularPalette "Blues" brewerBlues
-                , addTransform [Translate 5 12, Scale 4 4] $ wheel colors
-                , addTransform [Translate 14 12, Scale 4 4] $ wheel colors
+                , label (1,1) "Brewer Oranges"
+                , addTransform [Translate 0.5 0.5] $ rectangularPalette brewerOranges
+                , label (11,1) "Brewer Blues"
+                , addTransform [Translate 11 0.5] $ rectangularPalette brewerBlues
+                , label (1,9) "RYB Color Wheel"
+                , addTransform [Translate 5 14, Scale 4 4] $ wheel rybColors
+                , label (11,9) "10 constrasting color from d3"
+                , addTransform [Translate 15 14, Scale 4 4] $ wheel d3_10
                 ]
 
 style :: Element
 style = fromRight $ Right Default.style
    >>= addText ".label {font-size: 1px}"
 
-rectangularPalette :: T.Text -> [Color] -> Element
-rectangularPalette name cs = fromRight $ Right Default.g
-           >>= addChildren (label:palette)
-    where label = fromRight (Right Default.text >>= Text.y 0.5 >>= Text.class' ["label"] >>= addText name)
-          palette =map (\(i, color)->
+
+label :: (Double,Double) -> T.Text -> Element
+label (x,y) t = fromRight $ Right Default.text
+   >>= Text.class' ["label"]
+   >>= Text.x x
+   >>= Text.y y
+   >>= addText t
+
+
+rectangularPalette :: [Color] -> Element
+rectangularPalette cs = fromRight $ Right Default.g
+           >>= addChildren slices
+    --where label = fromRight (Right Default.text >>= Text.y 0.5 >>= Text.class' ["label"] >>= addText name)
+    where slices = map (\(i, color)->
                       fromRight (Right Default.rect
                          >>= Rect.x (fromIntegral i)
                          >>= Rect.y 1
@@ -62,10 +75,27 @@ brewerBlues =  ["#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#21
 colors :: [Color]
 colors = ["red", "blue", "green"]
 
+rybColors :: [Color]
+rybColors = [
+    "#ff0000", "#ff4900", "#ff7400", "#ff9200"
+  , "#ffaa00", "#ffbf00", "#ffd300", "#ffe800"
+  , "#ffff00", "#ccf600", "#9fee00", "#67e300"
+  , "#00cc00", "#00af64", "#009999", "#0b61a4"
+  , "#1240ab", "#1b1bb3", "#3914af", "#530fad"
+  , "#7109aa", "#a600a6", "#cd0074", "#e40045"
+  ]
+
+d3_10 :: [Color]
+d3_10 = [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"
+  , "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
+  ]
 
 wheel :: [Color] -> Element
 wheel cs = fromRight $ Right Default.g
-             >>= addChildren (map (slice (length cs)) $ zip [1..] cs)
+             >>= addChildren slices
+   --where label = fromRight $ Right Default.text >>= Text.class' ["label"] >>= addText t
+  where  slices = map (slice (length cs)) $ zip [1..] cs
 
 
 slice :: Int -> (Int, Color) -> Element
