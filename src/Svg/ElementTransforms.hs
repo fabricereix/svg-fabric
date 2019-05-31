99861fd6fa9bd5c724c5b0b70a0f690742554d99
Fabrice Reix <fabrice.reix@gmail.com>
2019-05-31 21:22:23 +0200

    plug your optimize path

--- src/Svg/ElementTransforms.hs
+++ src/Svg/ElementTransforms.hs
@@ -10,7 +10,7 @@ import qualified Data.Map as Map
 import qualified Data.Text as T
 
 -- import qualified Svg.Setter.Svg as SvgSetter
--- import Svg.Types.Core
+import Svg.Types.Core
 import Svg.Types.Format
 import Svg.Types.Parser
 
@@ -25,7 +25,16 @@ import Prelude hiding (id, length)
 
 
 mapAttributes :: (Name -> (Name,T.Text)->(Name,T.Text))-> (Element->Element)
-mapAttributes = undefined
+mapAttributes f Element {
+    elementName=element
+  , elementAttributes=attributes
+  , elementNodes=children
+  } = Element {
+     elementName=element
+   , elementAttributes=Map.fromList $ map (f element) $ Map.toList  attributes
+   , elementNodes=map (\case NodeElement element2 -> NodeElement $ mapAttributes f element2
+                             x -> x) children
+   }
 
 filterAttributes :: (Name -> (Name,T.Text)->Bool)-> (Element->Element)
 filterAttributes predicate Element {
@@ -46,17 +55,9 @@ removeDefault = filterAttributes defaultAttribute'
 removeUnknown :: Element -> Element
 removeUnknown = filterAttributes attributeExist
 
-
 normalizeValue :: Element -> Element
-normalizeValue element@Element {
-    elementName=name
-  , elementAttributes=attributes
-  , elementNodes=children
-  } = element {
-   elementAttributes=Map.fromList $ map (normalizeValue' name) $ Map.toList attributes
-   , elementNodes=map (\case NodeElement element2 -> NodeElement $ normalizeValue element2
-                             x -> x) children
-   }
+normalizeValue = mapAttributes normalizeValue'
+
 
 
 attributeExist :: Name -> (Name, T.Text) -> Bool
@@ -154,11 +155,40 @@ normalizeAttributeValue name _ _ = error $ "invalid element " ++ name
 
 
 optimizePaths :: Element -> Element
-optimizePaths element@Element{elementAttributes=attributes}
-  = element{elementAttributes=Map.fromList $ Map.toList attributes ++[newAttrib]}
-   where newAttrib = (Name {nameLocalName="x", nameNamespace=Nothing, namePrefix=Nothing}, "x")
+optimizePaths = mapAttributes optimizePathD
+
+optimizePathD ::  Name -> (Name, T.Text) -> (Name, T.Text)
+optimizePathD _ (name@Name {nameLocalName="d"}, s) = (
+    name
+  , case path (cs s) of
+      Left e -> error e
+      Right v -> cs $ formatPath $ optimizePath' v
+  )
+optimizePathD _ attribute = attribute
 
 
+optimizePath' :: Path -> Path
+optimizePath' (Path xs) = Path $ optimizePath xs
+
+
+optimizePath :: [Command] -> [Command]
+optimizePath [] = []
+optimizePath [c] = [c]
+optimizePath (c1:c2:css) = case mergeCommands c1 c2 of
+   [_,_] -> c1:optimizePath (c2:css)
+   c     -> optimizePath (c ++ css)
+
+
+mergeCommands :: Command -> Command -> [Command]
+mergeCommands c1@(H True x1) c2@(H True x2) = if signum x1 == signum x2
+                                              then [H True (x1+x2)]
+                                              else [c1,c2]
+mergeCommands c1@(V True x1) c2@(V True x2) = if signum x1 == signum x2
+                                              then [V True (x1+x2)]
+                                              else [c1,c2]
+
+mergeCommands (M True x1 y1) (M True x2 y2) = [M True (x1+x2) (y1+y2)]
+mergeCommands c1 c2 = [c1, c2]
 
 
 
